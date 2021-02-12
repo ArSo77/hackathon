bffcef076e3c1b18227cc8b8916abafbdb2cda60
ArSo77 <arsobieraj@gmail.com>
2021-02-12 20:51:38 +0100

    Test layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <router-view />
+  <MainLayout />
 </template>
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
+import MainLayout from "./layouts/MainLayout.vue";
 
 export default defineComponent({
-  name: 'App'
-})
+  name: "App",
+  components: { MainLayout },
+});
 </script>
