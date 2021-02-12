98ac66a7e4892addd444eed7799ede97e3c4eff1
ArSo77 <arsobieraj@gmail.com>
2021-02-12 21:27:27 +0100

    test

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -6,6 +6,8 @@ import routes from './routes'
  * If not building with SSR mode, you can
  * directly export the Router instantiation;
  *
+ * 
+ * 
  * The function below can be async too; either use
  * async/await or return a Promise which resolves
  * with the Router instance.
