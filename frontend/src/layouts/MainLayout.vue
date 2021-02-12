fba76f5467e98aa58cdad4baee48837a77752118
Mateusz Michalowski <matmic.michalowski@gmail.com>
2021-02-12 23:47:53 +0100

    FEATURE: dodanie menu bocznego z opcją przejścia do inneg routa i zaznaczaniem aktywnego routa.

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,5 +1,30 @@
 <template>
   <q-layout view="lHh Lpr lFf" class="bg-white">
+    <q-drawer
+      v-model="drawer"
+      show-if-above
+      :width="200"
+      :breakpoint="500"
+      class="sidebar-shadow"
+    >
+      <q-scroll-area class="fit">
+        <q-list padding class="menu-list">
+          <template v-for="menuOption in MENU_OPTIONS"
+                    :key="menuOption.name">
+            <q-item clickable v-ripple
+                    :active="currentActiveRoute(menuOption.path)"
+                    @click="$router.push(menuOption.path)">
+              <q-item-section avatar>
+                <q-icon :name="menuOption.icon" />
+              </q-item-section>
+              <q-item-section>
+                {{ menuOption.name }}
+              </q-item-section>
+            </q-item>
+          </template>
+        </q-list>
+      </q-scroll-area>
+    </q-drawer>
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -7,13 +32,27 @@
 </template>
 
 <script>
+import {MENU_OPTIONS} from "layouts/route-definitions";
+
 export default {
   name: "MyLayout",
 
   data() {
     return {
-      leftDrawerOpen: false,
+      drawer: true,
+      MENU_OPTIONS
     };
   },
+  methods: {
+    currentActiveRoute(selectedPath) {
+      return this.$route.path === selectedPath;
+    }
+  }
 };
 </script>
+<style>
+  .sidebar-shadow {
+    border-right: 1px solid rgba(0,0,0,.1);
+    box-shadow: 2px 0px 4px 0px rgba(0,0,0,.1);
+  }
+</style>
---
+++ frontend/src/layouts/route-definitions.js
@@ -0,0 +1,4 @@
+export const MENU_OPTIONS = [
+  { icon: '', name: 'Progress', path: '/progress' },
+  { icon: '', name: 'Register Wizard', path: '/register' },
+]
